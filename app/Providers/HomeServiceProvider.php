3a53a12e914477d69d28e764be07e7ae1e533a0d
sanath <sanathdavis@gmail.com>
2018-04-11 18:21:52 +0530

    terrible

--- app/Providers/HomeServiceProvider.php
+++ app/Providers/HomeServiceProvider.php
@@ -18,7 +18,7 @@ class HomeServiceProvider extends ServiceProvider
     public function boot()
     {
         view()->composer('home', function ($view) {
-            $matches = Match::orderBy('date', 'ASC')->get();
+            $matches = Match::orderBy('date', 'ASC')->get();            
             $teams = Team::all()->keyBy('id');
             $predictions = DB::table('predictions')->get();
             $userPredictions = DB::table('user_match_predictions')->where([['user_id', Auth::id()],['match_id',0]])->pluck('prediction', 'prediction_id')->toArray();
