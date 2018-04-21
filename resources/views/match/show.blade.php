bb5cace5835a51e4348c51ceef55883ba7ee1417
jithtitan <kevinred112@gmail.com>
2018-04-21 12:54:31 +0530

    individual match april 21

--- resources/views/match/show.blade.php
+++ resources/views/match/show.blade.php
@@ -1,22 +1,593 @@
-@extends('layouts.app')
+<!DOCTYPE HTML>
 
-@section('content')
-	<div class="container">
-    	<div class="row justify-content-center">
-			<div class="col-md-8">							
-				<div class="col-md-3 text-center">
-				<div class="d-flex justify-content-start">
-					<span class="flag-icon flag-icon-{{$teams[$match->home_team - 1]->iso2}}" style="transform: scale(2.5,2.5)"></span>					
-						<h3>{{$teams[$match->home_team - 1]->name}}</h3>					
-						</div>
-				<div class="d-flex justify-content-start">
-				
-				
-					<span class="flag-icon flag-icon-{{$teams[$match->away_team - 1]->iso2}}" style="transform: scale(2.5,2.5)"></span>					
-						<h3>{{$teams[$match->away_team - 1]->name}}</h3>					
-						</div>
-				</div>
-			</div>
-		</div>
-	</div>
-@endsection()
+ <html>
+
+    <head>
+
+    	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+
+        <meta charset="utf-8">
+
+        <!-- Description, Keywords and Author -->
+
+        <meta name="description" content="">
+
+        <meta name="author" content="">
+
+
+
+        <title>:: avana LLC ::</title>
+
+		<link rel="shortcut icon" href="/avana/images/favicon.ico" type="image/x-icon">
+
+
+
+        <!-- style -->
+
+        <link href="/avana/css/style.css" rel="stylesheet" type="text/css">
+
+        <!-- style -->
+
+        <!-- bootstrap -->
+
+        <link href="/avana/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+
+        <!-- responsive -->
+
+        <link href="/avana/css/responsive.css" rel="stylesheet" type="text/css">
+
+        <!-- font-awesome -->
+
+        <link href="/avana/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+        <!-- font-awesome -->
+
+        <link href="/avana/css/effects/set2.css" rel="stylesheet" type="text/css">
+
+        <link href="/avana/css/effects/normalize.css" rel="stylesheet" type="text/css">
+
+        <link href="/avana/css/effects/component.css"  rel="stylesheet" type="text/css" >
+
+	</head>
+
+
+
+    <body>
+
+
+
+    	<!-- header -->
+
+        	<!-- <header role="header">
+
+            	<div class="container">
+
+                	<!-- logo -->
+
+                    	<!-- <h1>
+
+                        	<a href="index.html" title="avana LLC"><img src="images/logo.png" title="avana LLC" alt="avana LLC"/></a>
+
+                        </h1> -->
+
+                    <!-- logo -->
+
+                    <!-- nav -->
+
+<!--                     <nav role="header-nav" class="navy">
+
+                        <ul>
+
+                            <li class="nav-active"><a href="index.html" title="Work">Work</a></li>
+
+                            <li><a href="about.html" title="About">About</a></li>
+
+                            <li><a href="blog.html" title="Blog">Blog</a></li>
+
+                            <li><a href="contact.html" title="Contact">Contact</a></li>
+
+                        </ul>
+
+                    </nav>
+
+ -->                    <!-- nav -->
+
+<!--                 </div>
+
+            </header> -->
+
+        <!-- header -->
+
+        <!-- main -->
+
+        <main role="main-home-wrapper" class="container">
+
+
+
+            <div class="row">
+
+
+
+            	<section class="col-xs-12 col-sm-6 col-md-6 col-lg-6 ">
+
+                	<article role="pge-title-content">
+
+                    	<header>
+
+                        	<h2><span>{{$match->type}}</span></h2> <h2>{{$teams[$match->home_team - 1]->name}}</h2>
+                            <h1>v/s</h1>
+                            <h2>{{$teams[$match->away_team - 1]->name}}</h2>
+
+                        </header>
+
+                        <p>{{$stadia[$match->stadium_id]->name}}</p>
+
+                    </article>
+
+                </section>
+
+
+
+                <section class="col-xs-12 col-sm-6 col-md-6 col-lg-6 grid">
+
+                	<figure class="effect-oscar">
+
+                    	<img src="/avana/images/home-images/image-1.jpg" alt="" class="img-responsive"/>
+
+                        <figcaption>
+
+                        	<h2>Eliana Dedda<span> Identity</span></h2>
+
+							<p>Personal Brand Identity.</p>
+
+							<a href="works-details.html">View more</a>
+
+                        </figcaption>
+
+                    </figure>
+
+                </section>
+
+
+
+                <div class="clearfix"></div>
+
+
+
+                <section class="col-xs-12 col-sm-6 col-md-6 col-lg-6 grid">
+
+                	<ul class="grid-lod effect-2" id="grid">
+
+                    	<li>
+
+                        	<figure class="effect-oscar">
+
+                            <img src="/avana/images/home-images/image-2.jpg" alt="" class="img-responsive"/>
+                            <img src="/avana/images/home-images/image-2.jpg" alt="" class="img-responsive"/>
+
+                            <figcaption>
+
+
+
+                                    <h2>{{$teams[$match->home_team - 1]->name}} <span>{{$teams[$match->away_team - 1]->name}}</span></h2>
+
+                                    <p>Project for Thonik, design studio based in Amsterdam</p>
+
+                                    <a href="works-details.html">View more</a>
+
+
+
+                            </figcaption>
+
+                        </figure>
+
+                        </li>
+
+
+
+                        <li>
+
+                        	<figure class="effect-oscar">
+
+                            <img src="images/home-images/image-4.jpg" alt="" class="img-responsive"/>
+
+                            <figcaption>
+
+                                <h2>A Brand <span>new Agency</span></h2>
+
+                                <p>Over 40,000 customers use our themes to power their</p>
+
+                                <a href="works-details.html">View more</a>
+
+                            </figcaption>
+
+                        </figure>
+
+                        </li>
+
+                        <li>
+
+                        	<figure class="effect-oscar">
+
+                            <img src="images/home-images/image-2.jpg" alt="" class="img-responsive"/>
+
+                             <figcaption>
+
+                                <h2>Studio Thonik <span>Exhibition</span></h2>
+
+                                    <p>Project for Thonik, design studio based in Amsterdam</p>
+
+                                <a href="works-details.html">View more</a>
+
+                            </figcaption>
+
+                        </figure>
+
+                        </li>
+
+                        <li>
+
+                        	<figure class="effect-oscar">
+
+                            <img src="images/home-images/image-4.jpg" alt="" class="img-responsive"/>
+
+                             <figcaption>
+
+                                <h2>A Brand <span>new Agency</span></h2>
+
+                                <p>Over 40,000 customers use our themes to power their</p>
+
+                                <a href="works-details.html">View more</a>
+
+                            </figcaption>
+
+                        </figure>
+
+                        </li>
+
+                        <li>
+
+                        	<figure class="effect-oscar">
+
+                            <img src="images/home-images/image-2.jpg" alt="" class="img-responsive"/>
+
+                             <figcaption>
+
+                                <h2>Studio Thonik <span>Exhibition</span></h2>
+
+                                    <p>Project for Thonik, design studio based in Amsterdam</p>
+
+                                <a href="works-details.html">View more</a>
+
+                            </figcaption>
+
+                        </figure>
+
+                        </li>
+
+                        <li>
+
+                        	<figure class="effect-oscar">
+
+                            <img src="images/home-images/image-4.jpg" alt="" class="img-responsive"/>
+
+                             <figcaption>
+
+                                <h2>A Brand <span>new Agency</span></h2>
+
+                                <p>Over 40,000 customers use our themes to power their</p>
+
+                                <a href="works-details.html">View more</a>
+
+                            </figcaption>
+
+                        </figure>
+
+                        </li>
+
+                        <li>
+
+                        	<figure class="effect-oscar">
+
+                            <img src="images/home-images/image-2.jpg" alt="" class="img-responsive"/>
+
+                             <figcaption>
+
+                                <h2>Studio Thonik <span>Exhibition</span></h2>
+
+                                    <p>Project for Thonik, design studio based in Amsterdam</p>
+
+                                <a href="works-details.html">View more</a>
+
+                            </figcaption>
+
+                        </figure>
+
+                        </li>
+
+                        <li>
+
+                        	<figure class="effect-oscar">
+
+                            <img src="images/home-images/image-4.jpg" alt="" class="img-responsive"/>
+
+                             <figcaption>
+
+                                <h2>A Brand <span>new Agency</span></h2>
+
+                                <p>Over 40,000 customers use our themes to power their</p>
+
+                                <a href="works-details.html">View more</a>
+
+                            </figcaption>
+
+                        </figure>
+
+                        </li>
+
+                    </ul>
+
+                </section>
+
+
+
+                <section class="col-xs-12 col-sm-6 col-md-6 col-lg-6 grid">
+
+                	<ul class="grid-lod effect-2" id="grid">
+
+                    	<li>
+
+                        	<figure class="effect-oscar">
+
+                            <img src="images/home-images/image-3.jpg" alt="" class="img-responsive"/>
+
+                             <figcaption>
+
+                                <h2>Anatome Milano <span>Galleria</span></h2>
+
+                                <p>Galerie Anatome based in Paris</p>
+
+                                <a href="works-details.html">View more</a>
+
+                            </figcaption>
+
+                        </figure>
+
+                        </li>
+
+                        <li>
+
+                        	<figure class="effect-oscar">
+
+                            <img src="images/home-images/image-5.jpg" alt="" class="img-responsive"/>
+
+                             <figcaption>
+
+                                <h2>A Brand <span>new Agency</span></h2>
+
+                                <p>Over 40,000 customers use our themes to power their</p>
+
+                                <a href="works-details.html">View more</a>
+
+                            </figcaption>
+
+                        </figure>
+
+                        </li>
+
+                        <li>
+
+                        	<figure class="effect-oscar">
+
+                            <img src="images/home-images/image-3.jpg" alt="" class="img-responsive"/>
+
+                             <figcaption>
+
+                                <h2>Anatome Milano <span>Galleria</span></h2>
+
+                                <p>Galerie Anatome based in Paris</p>
+
+                                <a href="works-details.html">View more</a>
+
+                            </figcaption>
+
+                        </figure>
+
+                        </li>
+
+                        <li>
+
+                        	<figure class="effect-oscar">
+
+                            <img src="images/home-images/image-5.jpg" alt="" class="img-responsive"/>
+
+                             <figcaption>
+
+                                <h2>A Brand <span>new Agency</span></h2>
+
+                                <p>Over 40,000 customers use our themes to power their</p>
+
+                                <a href="works-details.html">View more</a>
+
+                            </figcaption>
+
+                        </figure>
+
+                        </li>
+
+                        <li>
+
+                        	<figure class="effect-oscar">
+
+                            <img src="images/home-images/image-3.jpg" alt="" class="img-responsive"/>
+
+                             <figcaption>
+
+                               <h2>Anatome Milano <span>Galleria</span></h2>
+
+                                <p>Galerie Anatome based in Paris</p>
+
+                                <a href="works-details.html">View more</a>
+
+                            </figcaption>
+
+                        </figure>
+
+                        </li>
+
+                        <li>
+
+                             <figure class="effect-oscar">
+
+                                <img src="/avana/images/home-images/image-5.jpg" alt="" class="img-responsive"/>
+
+                                 <figcaption>
+
+                                <h2>A Brand <span>new Agency</span></h2>
+
+                                <p>Over 40,000 customers use our themes to power their</p>
+
+                                <a href="works-details.html">View more</a>
+
+                            </figcaption>
+
+                            </figure>
+
+                        </li>
+
+                        <li>
+
+                       	 	<figure class="effect-oscar">
+
+                            <img src="images/home-images/image-3.jpg" alt="" class="img-responsive"/>
+
+                             <figcaption>
+
+                                <h2>Anatome Milano <span>Galleria</span></h2>
+
+                                <p>Galerie Anatome based in Paris</p>
+
+                                <a href="works-details.html">View more</a>
+
+                            </figcaption>
+
+                        </figure>
+
+                        </li>
+
+                        <li>
+
+                        	<figure class="effect-oscar">
+
+                            <img src="images/home-images/image-5.jpg" alt="" class="img-responsive"/>
+
+                             <figcaption>
+
+                                <h2>A Brand <span>new Agency</span></h2>
+
+                                <p>Over 40,000 customers use our themes to power their</p>
+
+                                <a href="works-details.html">View more</a>
+
+                            </figcaption>
+
+                        </figure>
+
+                        </li>
+
+                    </ul>
+
+                </section>
+
+                <div class="clearfix"></div>
+
+            </div>
+
+        </main>
+
+    	<!-- main -->
+
+        <!-- footer -->
+
+        <!-- <footer role="footer">
+
+            <!-- logo -->
+<!--
+                <h1>
+
+                    <a href="index.html" title="avana LLC"><img src="images/logo.png" title="avana LLC" alt="avana LLC"/></a>
+
+                </h1>
+ -->
+            <!-- logo -->
+
+            <!-- nav -->
+<!--
+            <nav role="footer-nav">
+
+            	<ul>
+
+                	<li><a href="index.html" title="Work">Work</a></li>
+
+                    <li><a href="about.html" title="About">About</a></li>
+
+                    <li><a href="blog.html" title="Blog">Blog</a></li>
+
+                    <li><a href="contact.html" title="Contact">Contact</a></li>
+
+                </ul>
+
+            </nav> -->
+
+            <!-- nav -->
+<!--
+            <ul role="social-icons">
+
+            	<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+
+                <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+
+                <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+
+                <li><a href="#"><i class="fa fa-flickr" aria-hidden="true"></i></a></li>
+
+            </ul>
+
+            <p class="copy-right">&copy; 2015  avana LLC.. All rights Resved</p>
+
+        </footer> -->
+         <!-- footer -->
+
+
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+
+        <script src="/avana/js/jquery.min.js" type="text/javascript"></script>
+
+        <!-- custom -->
+
+		<script src="/avana/js/nav.js" type="text/javascript"></script>
+
+        <script src="/avana/js/custom.js" type="text/javascript"></script>
+
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+
+        <script src="/avana/js/bootstrap.min.js" type="text/javascript"></script>
+
+        <script src="/avana/js/effects/masonry.pkgd.min.js"  type="text/javascript"></script>
+
+		<script src="/avana/js/effects/imagesloaded.js"  type="text/javascript"></script>
+
+		<script src="/avana/js/effects/classie.js"  type="text/javascript"></script>
+
+		<script src="/avana/js/effects/AnimOnScroll.js"  type="text/javascript"></script>
+
+        <script src="/avana/js/effects/modernizr.custom.js"></script>
+
+        <!-- jquery.countdown -->
+
+        <script src="/avana/js/html5shiv.js" type="text/javascript"></script>
+
+    </body>
+
+</html>
