ba10c5efe65af71e279bae0a1b6b3915e435e92a
sanath <sanathdavis@gmail.com>
2018-06-16 15:29:12 +0530

    footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,22 +1,16 @@
 <footer class="footer text-center">
 	<div class="container">
-		{{-- <ul class="list-inline mb-5">
+		<ul class="list-inline mb-5">			
 			<li class="list-inline-item">
-				<a class="social-link rounded-circle text-white mr-3" href="#">
-					<i class="icon-social-facebook"></i>
+				<a target="_blank" title="YNWA" href="https://tenor.com/view/liverpool-fans-fc-gif-11608193">
+					<img src="/footer_img/liverpool.jpg">
 				</a>
 			</li>
 			<li class="list-inline-item">
-				<a class="social-link rounded-circle text-white mr-3" href="#">
-					<i class="icon-social-twitter"></i>
+				<a target="_blank" title="GGMU" href="https://tenor.com/view/pogba-paulpogba-soccer-football-gif-11839130">
+					<img src="/footer_img/united.png">
 				</a>
 			</li>
-			<li class="list-inline-item">
-				<a class="social-link rounded-circle text-white" href="#">
-					<i class="icon-social-github"></i>
-				</a>
-			</li>
-		</ul> --}}
-		{{-- <p class="text-muted small mb-0">Copyright &copy; FIFAPREDICT</p> --}}
+		</ul>
 	</div>
 </footer>
